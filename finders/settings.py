fcc854236821db1d7b2d21d39b4287d55ab4fa86
sayad-mallllek <bob@khalilelzein.com>
2024-05-19 00:27:54 +0300

    whitenoise test

--- finders/settings.py
+++ finders/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "finders.urls"
@@ -119,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
 # Default primary key field type
--- finders/wsgi.py
+++ finders/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "finders.settings")
 
 app = get_wsgi_application()
+
+app = WhiteNoise(app, root="ui/staticfiles")
