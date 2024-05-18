5d40e8a707dc97e19e8da4bbba580620e7cea7f3
sayad-mallllek <bob@khalilelzein.com>
2024-05-19 00:17:20 +0300

    some test?

--- finders/urls.py
+++ finders/urls.py
@@ -21,8 +21,5 @@ from django.conf.urls.static import static
 from finders import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("", admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
