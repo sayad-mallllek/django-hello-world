43a416f5d26e33c7934c557e24eb0d0bd2182302
sayad-mallllek <bob@khalilelzein.com>
2024-06-21 22:42:46 +0300

    feat: added expense category

--- expenses/admin.py
+++ expenses/admin.py
@@ -2,14 +2,21 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import reverse
 
-from expenses.models import Expense, Capital
+from expenses.models import Expense, ExpenseCategory, Capital
 from utils.models import BaseAdminModel
 
 
 # Register your models here.
+
+
+@admin.register(ExpenseCategory)
+class ExpenseCategoryAdmin(BaseAdminModel):
+    pass
+
+
 @admin.register(Expense)
 class ExpenseAdmin(BaseAdminModel):
-    pass
+    list_filter = ("category",)
 
 
 @admin.register(Capital)
